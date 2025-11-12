b517d76b2e2b065b4a80d8a4c8454f9379a3861d
Hirthuk <sharankumarp02@gmail.com>
2025-11-12 10:56:21 +0530

    Function Functional Interface completed with it's method

--- src/Part_5/FunctionFInterface.java
+++ src/Part_5/FunctionFInterface.java
@@ -19,7 +19,33 @@ public class FunctionFInterface {
         String upperCase = functionFI2.apply("Sharan Kumar");
 
         System.out.println(upperCase);
-    }
+
+        Function<String, String> VerifyNull =
+                (str) -> str.length() == 0 ? "Enter a Valid String" : str;
+
+        //    Using andThen to convert to Uppercase and checking is it not Null
+//        First Method will be executed post that second method will be executed
+        String result1 = VerifyNull.andThen(functionFI2).apply("Upper Case converted using andThen");
+
+        System.out.println(result1);
+
+
+//        compare function -> Just Opposite to andThen Function..
+//        If we apply the same logic as andTHen function above. We just need to reverse the function place
+//        Here VerifyNull will be executed first post that functionFI2
+        String result3 = functionFI2.compose(VerifyNull).apply("UpperCase using Compose");
+        System.out.println("Compose FUnction " + result3);
+
+        //        Identitiy Method
+        Function<Integer, Integer> identicalValue = Function.identity();
+        int value = identicalValue.apply(100);
+        System.out.println(value);
+
+
+
+    };
+
+
 
 //
 }
